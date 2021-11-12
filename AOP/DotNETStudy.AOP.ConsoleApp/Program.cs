29a004c60f33f18f60c92a01f51a7b3c1f8fa795
Liu Liang <lliang@outlook.com>
2021-11-12 10:41:11 +0800

    Add `DotNET.Study.AOP.ConsoleApp`.

---
+++ AOP/DotNETStudy.AOP.ConsoleApp/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.IO;
+
+namespace DotNETStudy.AOP.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var proxy = new MyService2Proxy(new Interceptor());
+                proxy.GetFileStream("xxx.xxx");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// 存在问题：没有参数检查逻辑，程序不健壮
+    /// 待实现功能：需要给调用者提示，并记录日志
+    /// </summary>
+    public class MyService
+    {
+        public int Calc(int a, int b)
+        {
+            // 如果 a 是 0 会抛异常
+            return (a + b) / a; // 主要的业务逻辑
+        }
+
+        public Stream GetFileStream(string filePath)
+        {
+            // 如果文件路径不存在会抛异常
+            return new FileStream(filePath, FileMode.Open);// 主要的业务逻辑
+        }
+    }
+
+    #region 方案1: 参数检查 或者 异常捕获
+
+    /// <summary>
+    /// 方案1
+    /// 存在不足：判断逻辑或者 try-catch 代码重复。
+    /// </summary>
+    public class MyService1
+    {
+        public int Calc(int a, int b)
+        {
+            if (a == 0)
+            {
+                throw new Exception("参数不能为0");
+            }
+
+            return (a + b) / a;
+        }
+
+        public Stream GetFileStream(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception("文件路径不存在");
+            }
+
+            return new FileStream(filePath, FileMode.Open);
+        }
+
+        public int Calc1(int a, int b)
+        {
+            try
+            {
+                return (a + b) / a;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);// 输出错误信息：系统级别的逻辑
+                throw new Exception("错误，请稍后再试一下");
+            }
+        }
+
+        public Stream GetFileStream1(string filePath)
+        {
+            try
+            {
+                return new FileStream(filePath, FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);// 输出错误信息：系统级别的逻辑
+                throw new Exception("错误，请稍后再试一下");
+            }
+        }
+    }
+
+    #endregion
+
+    #region 方案2：代理类 + 拦截器
+
+    public class MyService2
+    {
+        public virtual void Calc(int a, int b)
+        {
+            // 如果 a 是 0 会抛异常
+            Console.WriteLine((a + b) / a); // 主要的业务逻辑
+        }
+
+        public virtual void GetFileStream(string filePath)
+        {
+            // 如果文件路径不存在会抛异常
+            var stream = new FileStream(filePath, FileMode.Open);// 主要的业务逻辑
+            Console.WriteLine($"Stream length: {stream.Length}");
+        }
+    }
+
+    /// <summary>
+    /// 生成代理类，子类重写父类方法
+    /// </summary>
+    public class MyService2Proxy : MyService2
+    {
+        readonly Interceptor _interceptor;
+
+        public MyService2Proxy(Interceptor interceptor)
+        {
+            _interceptor = interceptor;
+        }
+
+        public override void Calc(int a, int b)
+        {
+            var invoke = new Invoketion(() => base.Calc(a, b));
+
+            _interceptor.Intercept(invoke);
+        }
+
+        public override void GetFileStream(string filePath)
+        {
+            var invoke = new Invoketion(() => base.GetFileStream(filePath));
+
+            _interceptor.Intercept(invoke);
+        }
+    }
+
+    public class Invoketion
+    {
+        Action _action;
+
+        public Invoketion(Action action)
+        {
+            _action = action;
+        }
+
+        public object Target { get; set; }
+
+        public void Proceed()
+        {
+            _action();
+        }
+    }
+
+    /// <summary>
+    /// 拦截器
+    /// </summary>
+    public class Interceptor
+    {
+        /// <summary>
+        /// 拦截
+        /// </summary>
+        /// <param name="invoketion"></param>
+        /// <exception cref="Exception"></exception>
+        public void Intercept(Invoketion invoketion)
+        {
+            try
+            {
+                invoketion.Proceed();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);// 输出错误信息：系统级别的逻辑
+                throw new Exception("错误，请稍后再试一下");
+            }
+        }
+    }
+
+    #endregion
+}
