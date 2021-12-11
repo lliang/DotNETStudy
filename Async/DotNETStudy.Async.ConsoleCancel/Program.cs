11fab816d24d6d752cc14c7b914b99ec5692a1a5
Liu Liang <lliang@outlook.com>
2021-12-11 16:05:11 +0800

    Add `CancellationToke comments` and  add `explicit operator` code.

--- Async/DotNETStudy.Async.ConsoleCancel/Program.cs
+++ Async/DotNETStudy.Async.ConsoleCancel/Program.cs
@@ -3,6 +3,22 @@ using System.Threading.Tasks;
 
 namespace DotNETStudy.Async.ConsoleCancel
 {
+    /*
+        CancellationToken 结构体
+
+        None：空
+
+        bool IsCancellationRequested 是否取消
+        (*)Register(Action callback) 注册取消监听
+        ThrowIfCancellationRequested() 如果任务被取消，执行到这就抛异常。
+
+        Thread.Abort()
+
+        CancellationTokenSource
+        CancelAfter() 超时后发出取消信号
+        Cancel() 发出取消信号
+        CancellationToken Token
+     */
     class Program
     {
         static void Main(string[] args)
--- IoC/DotNETStudy.IoC.ConsoleIoC/Program.cs
+++ IoC/DotNETStudy.IoC.ConsoleIoC/Program.cs
@@ -4,6 +4,28 @@
     {
         static void Main(string[] args)
         {
+            Stone stone = new Stone { Age = 5000 };
+            Monkey monkey = (Monkey)stone;
+            System.Console.WriteLine(monkey.Age);
         }
+    }
+
+    class Stone
+    {
+        public int Age { get; set; }
+
+        public static explicit operator Monkey(Stone stone)
+        {
+            Monkey monkey = new Monkey
+            {
+                Age = stone.Age / 500
+            };
+            return monkey;
+        }
+    }
+
+    class Monkey
+    {
+        public int Age { get; set; }
     }
 }
