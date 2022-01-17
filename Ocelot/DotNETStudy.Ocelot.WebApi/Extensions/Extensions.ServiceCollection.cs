ed6139a581e844d0f17191fc72b60b081fe05a2e
Liu Liang <lliang@outlook.com>
2022-01-17 15:21:59 +0800

    Add `DotNETStudy.Ocelot.WebApi`.

---
+++ Ocelot/DotNETStudy.Ocelot.WebApi/Extensions/Extensions.ServiceCollection.cs
@@ -0,0 +1,14 @@
+﻿using Ocelot.Provider.Consul;
+using Ocelot.DependencyInjection;
+
+namespace DotNETStudy.Ocelot.WebApi.Extensions
+{
+    public static class Extensions
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+            services.AddOcelot(config).AddConsul();
+        }
+    }
+}
