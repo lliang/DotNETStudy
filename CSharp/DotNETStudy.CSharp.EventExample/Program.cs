cfa105d2887ef0879434ad7e2df156f7199a723b
Liu Liang <lliang@outlook.com>
2023-02-25 17:03:10 +0800

    Add EventExample and IspExample

---
+++ CSharp/DotNETStudy.CSharp.EventExample/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace DotNETStudy.CSharp.EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Waiter waiter = new Waiter();
+            customer.Order += waiter.Action;
+            customer.Action();
+            customer.PayTheBill();
+        }
+    }
+
+    class OrderEventArgs : EventArgs
+    {
+        public string DishName { get; set; }
+
+        public string Size { get; set; }
+    }
+
+    delegate void OrderEventHandler(Customer customer, OrderEventArgs e);
+
+    class Customer
+    {
+        private OrderEventHandler orderEventHandler;
+
+        public event OrderEventHandler Order
+        {
+            add
+            {
+                orderEventHandler += value;
+            }
+            remove
+            {
+                orderEventHandler -= value;
+            }
+        }
+
+        public double Bill { get; set; }
+
+        public void PayTheBill()
+        {
+            Console.WriteLine("I will pay ${0}.", Bill);
+        }
+
+        public void WalkIn()
+        {
+            Console.WriteLine("Walk into the restaurant.");
+        }
+
+        public void SitDown()
+        {
+            Console.WriteLine("Sit down.");
+        }
+
+        public void Think()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Let me think...");
+                Thread.Sleep(1000);
+            }
+
+            if (orderEventHandler != null)
+            {
+                OrderEventArgs e = new OrderEventArgs
+                {
+                    DishName = "kongpao Chicken",
+                    Size = "large"
+                };
+                orderEventHandler.Invoke(this, e);
+            }
+        }
+
+        public void Action()
+        {
+            Console.ReadLine();
+            WalkIn();
+            SitDown();
+            Think();
+        }
+    }
+
+    class Waiter
+    {
+        public void Action(Customer customer, OrderEventArgs e)
+        {
+            Console.WriteLine("I will serve you the dish - {0}.", e.DishName);
+            double price = 10;
+            switch (e.Size)
+            {
+                case "small":
+                    price *= 0.5;
+                    break;
+                case "large":
+                    price *= 1.5;
+                    break;
+                default:
+                    break;
+            }
+
+            customer.Bill += price;
+        }
+    }
+}
---
+++ CSharp/DotNETStudy.CSharp.IspExample/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+int[] nums1 = { 1, 2, 3, 4, 5 };
+var roc = new ReadOnlyCollection(nums1);
+foreach (var n in roc)
+{
+    Console.WriteLine(n);
+}
+
+class ReadOnlyCollection : IEnumerable
+{
+    private int[] _array;
+
+    public ReadOnlyCollection(int[] array)
+    {
+        _array = array;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return new Enumerator(this);
+    }
+
+    class Enumerator : IEnumerator
+    {
+        private ReadOnlyCollection _collection;
+        private int _index;
+
+        public Enumerator(ReadOnlyCollection collection)
+        {
+            _collection = collection;
+            _index = -1;
+        }
+
+        public object Current => _collection._array[_index];
+
+        public bool MoveNext()
+        {
+            if (++_index < _collection._array.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+    }
+}
